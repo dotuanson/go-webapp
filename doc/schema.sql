e8083259cf49b67552ee34a67dac96503042cdce
dotuanson <dotuansondn@gmail.com>
2023-11-07 20:57:48 +0700

    docs: add new schema SQL for users tables

--- doc/schema.sql
+++ doc/schema.sql
@@ -1,29 +1,40 @@
 CREATE SCHEMA "payment";
 
+CREATE TABLE "payment"."users" (
+    "username" varchar PRIMARY KEY,
+    "hashed_password" varchar NOT NULL,
+    "full_name" varchar NOT NULL,
+    "email" varchar UNIQUE NOT NULL,
+    "password_changed_at" timestamptz NOT NULL DEFAULT '0001-01-01 00:00:00Z',
+    "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
 CREATE TABLE "payment"."accounts" (
-  "id" bigserial PRIMARY KEY,
-  "owner" varchar NOT NULL,
-  "balance" bigint NOT NULL,
-  "currency" varchar NOT NULL,
-  "created_at" timestampz NOT NULL DEFAULT (now())
+    "id" bigserial PRIMARY KEY,
+    "owner" varchar NOT NULL,
+    "balance" bigint NOT NULL,
+    "currency" varchar NOT NULL,
+    "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE "payment"."entries" (
-  "id" bigserial PRIMARY KEY,
-  "account_id" bigint NOT NULL,
-  "amount" bigint NOT NULL,
-  "created_at" timestampz NOT NULL DEFAULT (now())
+    "id" bigserial PRIMARY KEY,
+    "account_id" bigint NOT NULL,
+    "amount" bigint NOT NULL,
+    "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE "payment"."transfers" (
-  "id" bigserial PRIMARY KEY,
-  "from_account_id" bigint NOT NULL,
-  "to_account_id" bigint NOT NULL,
-  "amount" bigint NOT NULL,
-  "created_at" timestampz NOT NULL DEFAULT (now())
+    "id" bigserial PRIMARY KEY,
+    "from_account_id" bigint NOT NULL,
+    "to_account_id" bigint NOT NULL,
+    "amount" bigint NOT NULL,
+    "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
 CREATE INDEX ON "payment"."accounts" ("owner");
+
+CREATE UNIQUE INDEX ON "payment"."accounts" ("owner", "currency");
 
 CREATE INDEX ON "payment"."entries" ("account_id");
 
@@ -36,6 +47,8 @@ CREATE INDEX ON "payment"."transfers" ("from_account_id", "to_account_id");
 COMMENT ON COLUMN "payment"."entries"."amount" IS 'can be negative or positive';
 
 COMMENT ON COLUMN "payment"."transfers"."amount" IS 'must be positive';
+
+ALTER TABLE "payment"."accounts" ADD FOREIGN KEY ("owner") REFERENCES "payment"."users" ("username");
 
 ALTER TABLE "payment"."entries" ADD FOREIGN KEY ("account_id") REFERENCES "payment"."accounts" ("id");
 
