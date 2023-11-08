2363c078d218673d12d5fdfa146cd5bbda6dd53b
dotuanson <dotuansondn@gmail.com>
2023-11-08 08:55:48 +0700

    feat: handle db errors

--- internal/payment/transport/api/account.go
+++ internal/payment/transport/api/account.go
@@ -3,7 +3,9 @@ package api
 import (
 	"database/sql"
 	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
 	db "go-webapp/db/sqlc"
+	"log"
 	"net/http"
 )
 
@@ -27,6 +29,14 @@ func (server *Server) createAccount(ctx *gin.Context) {
 
 	account, err := server.store.CreateAccount(ctx, arg)
 	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			log.Println(pqErr.Code.Name())
+			switch pqErr.Code.Name() {
+			case "foreign_key_violation", "unique_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
