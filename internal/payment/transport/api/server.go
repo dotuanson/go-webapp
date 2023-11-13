3a691688849fee342a14a36f3f946028de8ee15b
dotuanson <dotuansondn@gmail.com>
2023-11-14 05:56:38 +0700

    feat: implement refresh token API

--- internal/payment/transport/api/server.go
+++ internal/payment/transport/api/server.go
@@ -47,9 +47,9 @@ func (server *Server) setupRouter() error {
 
 	router.POST("/users", server.createUser)
 	router.POST("/users/login", server.loginUser)
+	router.POST("/users/renew_access", server.renewAccessToken)
 
 	authRoutes := router.Group("/").Use(authMiddleware(server.tokenMaker))
-
 	authRoutes.POST("/accounts", server.createAccount)
 	authRoutes.GET("/accounts/:id", server.getAccount)
 	authRoutes.GET("/accounts", server.listAccount)
